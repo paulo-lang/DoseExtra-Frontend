28bd9b43873b7d2e203218a4d47e65b8ae38f7d4
paulo-lang <paulo.santos02321@gmail.com>
2022-04-27 11:26:43 -0300

    Implemented About page

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,16 +1,71 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
+import image from '../../assets/Home/wallpaper.jpg'
 import './styles.css';
 
-
 function About() {
+  useEffect(() => {
+    var links = document.getElementsByClassName('link')
+    for (let i = 0; i < links.length; i++) {
+      links[i].style.color = "#5ae1e6"
+    }
+    document.getElementsByClassName('header')[0].style.backgroundColor = "#0b2047"
+  }, [])
+
   return (
-    <div>
-      <div>
-        <h2>Sobre nós...</h2>
-        <p>Bando de cuzao que toca musica</p>
-      </div>
+
+    <div className='container'>
+      <section id="sec-1">
+        <div className='title'>Membros</div>
+        <div className='members-container'>
+          <div className='member-container'>
+            <div className='member-sub-container'>
+
+              <div className='member-name'>Lorem Ipsum</div>
+
+              <img className='member-image' src={image} height={300} width={260} />
+              <div className='member-desc'>Guitarrista</div>
+            </div>
+          </div>
+
+          <div className='member-container'>
+            <div className='member-sub-container'>
+
+              <div className='member-name'>Lorem Ipsum</div>
+
+              <img className='member-image' src={image} height={300} width={260} />
+              <div className='member-desc'>Guitarrista</div>
+            </div>
+          </div>
+          <div className='member-container'>
+            <div className='member-sub-container'>
+
+              <div className='member-name'>Lorem Ipsum</div>
+
+              <img className='member-image' src={image} height={300} width={260} />
+              <div className='member-desc'>Guitarrista</div>
+            </div>
+          </div>
+          <div className='member-container'>
+            <div className='member-sub-container'>
+
+              <div className='member-name'>Lorem Ipsum</div>
+
+              <img className='member-image' src={image} height={300} width={260} />
+              <div className='member-desc'>Guitarrista</div>
+            </div>
+          </div>
+        </div>
+        <div className="scroll-down" onClick={()=>{window.scrollTo({top:document.body.scrollHeight, behavior:'smooth'})}}></div>
+      </section>
+      <section id="sec-2">
+        <div className='title-2'>Sobre nós</div>
+        <div className='band-desc'> Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum </div>
+      </section>
+
     </div>
+
+
   );
 }
 
--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,19 +1,68 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
+import image from '../../assets/Home/wallpaper.jpg'
 import './styles.css';
 
-
 function Contact() {
+  useEffect(() => {
+    var links = document.getElementsByClassName('link')
+    for (let i = 0; i < links.length; i++) {
+      links[i].style.color = "#5ae1e6"
+    }
+    document.getElementsByClassName('header')[0].style.backgroundColor = "#0b2047"
+  }, [])
+
   return (
 
-    <div>
+    <div className='container'>
 
-      <h2>Contato</h2>
-      <p>Pelo whatsapp:</p>
-      <p>(19) 98339-9763</p>
-      <p>(19) 98265-6408</p>
+      <div className='members-container'>
+        <div className='member-container'>
+          <div className='member-sub-container'>
+            <div className='member-name'>Lorem Ipsum</div>
 
-    </div>  
+            <img className='member-image' src={image} height={300} width={260} />
+            <div className='member-desc'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut venenatis congue enim ac rutrum. Quisque ac tellus nulla. Proin aliquam ornare ex non pretium. Aenean interdum varius vehicula. Donec venenatis odio eros, sed molestie nulla sollicitudin ac. Proin odio ante, maximus non molestie eget, viverra sed urna. Donec eget ultricies purus.
+
+              Vivamus bibendum turpis urna, eu egestas lectus venenatis et. Donec scelerisque est justo, vel pretium arcu faucibus eget. Integer sit amet lacus non quam sagittis malesuada. Aliquam erat volutpat. Mauris id convallis nisi, sed condimentum risus. In ultrices sem diam, in feugiat enim tincidunt ut. Duis hendrerit at odio malesuada faucibus. In vitae felis quis turpis ultricies vestibulum nec ut mauris. Mauris facilisis imperdiet turpis, at pellentesque felis posuere at. Mauris at sodales quam. Duis et est sit amet justo cursus dictum. In mattis erat in risus volutpat, quis iaculis neque mollis.</div>
+
+          </div>
+        </div>
+        <div className='member-container'>
+          <div className='member-sub-container'>
+            <div className='member-name'>Lorem Ipsum</div>
+
+            <img className='member-image' src={image} height={300} width={260} />
+            <div className='member-desc'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut venenatis congue enim ac rutrum. Quisque ac tellus nulla. Proin aliquam ornare ex non pretium. Aenean interdum varius vehicula. Donec venenatis odio eros, sed molestie nulla sollicitudin ac. Proin odio ante, maximus non molestie eget, viverra sed urna. Donec eget ultricies purus.
+
+              Vivamus bibendum turpis urna, eu egestas lectus venenatis et. Donec scelerisque est justo, vel pretium arcu faucibus eget. Integer sit amet lacus non quam sagittis malesuada. Aliquam erat volutpat. Mauris id convallis nisi, sed condimentum risus. In ultrices sem diam, in feugiat enim tincidunt ut. Duis hendrerit at odio malesuada faucibus. In vitae felis quis turpis ultricies vestibulum nec ut mauris. Mauris facilisis imperdiet turpis, at pellentesque felis posuere at. Mauris at sodales quam. Duis et est sit amet justo cursus dictum. In mattis erat in risus volutpat, quis iaculis neque mollis.</div>
+
+          </div>
+        </div>
+        <div className='member-container'>
+          <div className='member-sub-container'>
+            <div className='member-name'>Lorem Ipsum</div>
+
+            <img className='member-image' src={image} height={300} width={260} />
+            <div className='member-desc'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut venenatis congue enim ac rutrum. Quisque ac tellus nulla. Proin aliquam ornare ex non pretium. Aenean interdum varius vehicula. Donec venenatis odio eros, sed molestie nulla sollicitudin ac. Proin odio ante, maximus non molestie eget, viverra sed urna. Donec eget ultricies purus.
+
+              Vivamus bibendum turpis urna, eu egestas lectus venenatis et. Donec scelerisque est justo, vel pretium arcu faucibus eget. Integer sit amet lacus non quam sagittis malesuada. Aliquam erat volutpat. Mauris id convallis nisi, sed condimentum risus. In ultrices sem diam, in feugiat enim tincidunt ut. Duis hendrerit at odio malesuada faucibus. In vitae felis quis turpis ultricies vestibulum nec ut mauris. Mauris facilisis imperdiet turpis, at pellentesque felis posuere at. Mauris at sodales quam. Duis et est sit amet justo cursus dictum. In mattis erat in risus volutpat, quis iaculis neque mollis.</div>
+
+          </div></div>
+        <div className='member-container'>
+          <div className='member-sub-container'>
+            <div className='member-name'>Lorem Ipsum</div>
+
+            <img className='member-image' src={image} height={300} width={260} />
+            <div className='member-desc'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut venenatis congue enim ac rutrum. Quisque ac tellus nulla. Proin aliquam ornare ex non pretium. Aenean interdum varius vehicula. Donec venenatis odio eros, sed molestie nulla sollicitudin ac. Proin odio ante, maximus non molestie eget, viverra sed urna. Donec eget ultricies purus.
+
+              Vivamus bibendum turpis urna, eu egestas lectus venenatis et. Donec scelerisque est justo, vel pretium arcu faucibus eget. Integer sit amet lacus non quam sagittis malesuada. Aliquam erat volutpat. Mauris id convallis nisi, sed condimentum risus. In ultrices sem diam, in feugiat enim tincidunt ut. Duis hendrerit at odio malesuada faucibus. In vitae felis quis turpis ultricies vestibulum nec ut mauris. Mauris facilisis imperdiet turpis, at pellentesque felis posuere at. Mauris at sodales quam. Duis et est sit amet justo cursus dictum. In mattis erat in risus volutpat, quis iaculis neque mollis.</div>
+
+          </div>
+        </div>
+      </div>
+
+    </div>
 
   );
 }
